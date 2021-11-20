8f1647ac8c36d18425e10e2ac864e7dd5ba28c00
Amit Krishna A <amit.ananthkumar@gmail.com>
2021-11-20 13:06:48 +0530

    Parcel, Babel

--- Intro-Adopt-Me/src/App.js
+++ Intro-Adopt-Me/src/App.js
@@ -1,13 +1,6 @@
-/* global React ReactDOM*/
-
-// A react Component with props (Properties)
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Pet from'./Pet' 
 
 // A simple react Component
 const App = () => {
