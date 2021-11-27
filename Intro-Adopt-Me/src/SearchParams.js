75c616554b4dba8b0b67234e0d6d69105506109e
Amit Krishna A <amit.ananthkumar@gmail.com>
2021-11-27 13:19:11 +0530

    React Hooks Advanced

--- Intro-Adopt-Me/src/SearchParams.js
+++ Intro-Adopt-Me/src/SearchParams.js
@@ -1,14 +1,41 @@
 import { useState } from 'react'
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
     const [location, setLocation] = useState("Seattle, WA");
+    const [animal, setAnimal] = useState("rabbit")
 
     return (
         <div className="search-params">
             <form>
                 <label hrmlFor="location">
                     Location
-                    <input id="location" onChange={e => setLocation(e.target.value)} value={location} placeholder="Location"/>
+                    <input id="location" 
+                    onChange={e => setLocation(e.target.value)} 
+                    value={location} 
+                    placeholder="Location"/>
+                </label>
+
+
+                <label htmlFor="animal">
+                    Animal
+                    <select 
+                        id="animal"
+                        value={animal}
+                        onChange={e => setAnimal(e.target.value)}
+                        onBlur={e => setAnimal(e.target.value)}>
+
+                        
+                        {
+                            ANIMALS.map((animal) => (
+                                <option value={animal} key={animal}>
+                                    {animal}
+                                </option>
+                            ))
+                        }
+
+                    </select>
                 </label>
             </form>
         </div>
