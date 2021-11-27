4a37bd0464c61ff681c9094f2ff4f614807c9ccd
Amit Krishna A <amit.ananthkumar@gmail.com>
2021-11-27 23:14:36 +0530

    React router for link

--- Intro-Adopt-Me/src/App.js
+++ Intro-Adopt-Me/src/App.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom'
 import SearchParams from './SearchParams'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Details from './Details'
+import { Link } from "react-router-dom";
+
 
 // A simple react Component
 const App = () => {
     return (
         <div>
-            <h1 id="my-brand">Adopt Me</h1>
+
             <Router> 
+            <header>
+                {/* Use he link tag instead of a for hyperlinks to avoid restarting reactapp */}
+                <Link to="/"><h1 id="my-brand">Adopt Me</h1></Link>
+            </header>
                 <Switch>
                     {/* Pass Variable into the route like this ':[var]' */}
                     <Route path="/details/:id">
--- Intro-Adopt-Me/src/Pet.js
+++ Intro-Adopt-Me/src/Pet.js
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+
+
 // Using JSX
 const Pet = ({ 
     name,
@@ -15,7 +18,7 @@ const Pet = ({
     }
 
     return (
-        <a href={`/details/${id}`} className='pet'>
+        <Link to={`/details/${id}`} className='pet'>
             <div className="image-container">
                 <img src={hero} alt={name} />
             </div>
@@ -24,7 +27,7 @@ const Pet = ({
                 <h2>{`${animal} - ${breed} - ${location}`}-</h2>
             </div>
             
-        </a>
+        </Link>
     )
 }
 
