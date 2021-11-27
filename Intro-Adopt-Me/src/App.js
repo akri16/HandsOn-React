5203e0481d42ae31928317d4f9c3d336c7afb9b8
Amit Krishna A <amit.ananthkumar@gmail.com>
2021-11-27 18:28:58 +0530

    React router

--- Intro-Adopt-Me/src/App.js
+++ Intro-Adopt-Me/src/App.js
@@ -1,13 +1,26 @@
 import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
 import SearchParams from './SearchParams'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Details from './Details'
 
 // A simple react Component
 const App = () => {
     return (
         <div>
             <h1 id="my-brand">Adopt Me</h1>
-            <SearchParams/>
+            <Router> 
+                <Switch>
+                    {/* Pass Variable into the route like this ':[var]' */}
+                    <Route path="/details/:id">
+                        <Details/>
+                    </Route>
+                
+                    <Route path="/">
+                        <SearchParams/>
+                    </Route>
+                </Switch>
+            </Router>
         </div>
     );
 }
---
+++ Intro-Adopt-Me/src/Details.js
@@ -0,0 +1,2 @@
+const Details = () => ( <h2> WTFFFFFFFFF! </h2> );
+export default Details;
