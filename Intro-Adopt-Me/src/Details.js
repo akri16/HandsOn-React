4d0638460f8ab7eec70b2a88bf0087dec14cd883
Amit Krishna A <amit.ananthkumar@gmail.com>
2021-11-30 10:31:11 +0530

    Class Components

--- Intro-Adopt-Me/src/Details.js
+++ Intro-Adopt-Me/src/Details.js
@@ -1,2 +1,42 @@
-const Details = () => ( <h2> WTFFFFFFFFF! </h2> );
-export default Details;
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+
+class Details extends Component {
+
+    constructor () {
+        super();
+
+        this.state = { loading: true, otherThing: "hi there" };
+    }
+
+    async componentDidMount () {
+        const res = await fetch(
+            `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+        );
+
+        const json = await res.json();
+        this.setState(Object.assign({
+                loading: false
+            }, json.pets[0]
+        ));
+    }
+
+    render() {
+        const { animal, breed, name, city, state, description } = this.state;
+        console.log(this.state);
+        return (
+            <div className="details"> 
+                <div>
+                    <h1>{name}</h1>
+                    <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
+                    <button>Adopt {name}</button>
+                    <p>{description}</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+export default withRouter(Details);
