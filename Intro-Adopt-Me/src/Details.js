56a63096eccc10011a3c140f8f38ff1e909eec7e
Amit Krishna A <amit.ananthkumar@gmail.com>
2021-12-03 00:04:49 +0530

    Class Properties

--- Intro-Adopt-Me/src/Details.js
+++ Intro-Adopt-Me/src/Details.js
@@ -1,42 +1,40 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
 
-
 class Details extends Component {
 
-    constructor () {
-        super();
+  state = { loading: true, otherThing: "hi there" };
 
-        this.state = { loading: true, otherThing: "hi there" };
-    }
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+        
+    const json = await res.json();
+    this.setState(
+      Object.assign(
+        {
+          loading: false,
+        },
+        json.pets[0]
+      )
+    );
+  }
 
-    async componentDidMount () {
-        const res = await fetch(
-            `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
-        );
-
-        const json = await res.json();
-        this.setState(Object.assign({
-                loading: false
-            }, json.pets[0]
-        ));
-    }
-
-    render() {
-        const { animal, breed, name, city, state, description } = this.state;
-        console.log(this.state);
-        return (
-            <div className="details"> 
-                <div>
-                    <h1>{name}</h1>
-                    <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
-                    <button>Adopt {name}</button>
-                    <p>{description}</p>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    const { animal, breed, name, city, state, description } = this.state;
+    console.log(this.state);
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
 }
-
 
 export default withRouter(Details);
--- Intro-Adopt-Me/src/useBreedList.js
+++ Intro-Adopt-Me/src/useBreedList.js
@@ -4,6 +4,7 @@ const localCache = {};
 
 export default function useBreedList(animal) {
 
+    
     const [breedList, setBreedList] = useState([]);
     const [status, setStatus] = useState('unloaded');
 
