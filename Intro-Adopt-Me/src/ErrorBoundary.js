8bf51c63f3d1bbdb106ce6c9547db3a04293ada0
Amit Krishna A <amit.ananthkumar@gmail.com>
2021-12-09 08:37:50 +0530

    Redirect on Error

--- Intro-Adopt-Me/src/ErrorBoundary.js
+++ Intro-Adopt-Me/src/ErrorBoundary.js
@@ -1,10 +1,10 @@
 import { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
 
 class ErrorBoundary extends Component {
 
-    state = { hasError: false };
+    state = { hasError: false, redirect: false };
 
     static getDerivedStateFromError() {
         return { hasError: true }
@@ -13,11 +13,15 @@ class ErrorBoundary extends Component {
     componentDidCatch(error, info) {
         // Log this to Sentry, Azure Monitor ....
         console.error("Error Boundary caught an error");
+        setTimeout(() => this.setState({ redirect: true }), 5000);
     } 
 
 
     render () {
-        if (this.state.hasError) {
+
+        if (this.state.redirect) {
+            return <Redirect to="/" />
+        } else if (this.state.hasError) {
             return (
                 <h2>
                     This listing has an error. Click <Link to="/">here</Link> to go back to safety
